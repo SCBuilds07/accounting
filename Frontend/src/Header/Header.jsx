a8392374a1c8f6001cbe8d0bdd7275cf173dec4b
Tim van Baarle <timvanbaarle@outlook.com>
2026-08-17 15:18:45 +0200

    firebase config change and header change for test pt5 electric boogaloo

--- Frontend/src/Header/Header.jsx
+++ Frontend/src/Header/Header.jsx
@@ -3,7 +3,7 @@ import './Header.scss'
 function Header() {
     return (
             <header>
-                <h1>Boekhoud.nl</h1>
+                <h1>Boekhoud</h1>
                 <h2>You tried the best, now try the rest!</h2>
             </header>
     )
